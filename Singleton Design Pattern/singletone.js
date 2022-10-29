8db7ffd9eca23cf5071b9ae251fedd73b10fc41b
Chandan Ojha <chandanojha776@gmail.com>
2022-10-29 09:33:01 +0600

    singletone practice done

--- Singleton Design Pattern/singletone.js
+++ Singleton Design Pattern/singletone.js
@@ -16,3 +16,28 @@ let Singleton = (function (){
         }
     };
 })();*/
+
+/*Another Design Pattern*/
+var singleton = (function (){
+    var instance;
+
+    function createInstance(){
+        var object = new Object("I am the instance");
+        return object;
+    }
+    return{
+        getInstance: function (){
+            if(!instance){
+                instance = createInstance();
+            }
+            return instance;
+        }
+    };
+})();
+
+function run(){
+    var instance1 = singleton.getInstance();
+    var instance2 = singleton.getInstance();
+    console.log("Same instance? " + (instance1 === instance2));
+}
+run();
